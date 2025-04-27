efaa7050b37e045a5f46d2dd81a1e697949a910f
SangeetaWinner <sangeetawinner@gmail.com>
2025-04-27 18:52:28 +0530

    added render url

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = "https://your-render-url.onrender.com";
+const API_BASE_URL = "https://task-manager-app-server-rurm.onrender.com";
 
 export const getTasks = () => fetch(`${API_BASE_URL}/tasks`).then(res => res.json());
 export const addTask = (task) => fetch(`${API_BASE_URL}/tasks`, {
